234c53c37c581d3dbdf1062b75bbafc3d98d05e0
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-20 21:37:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    already_exists = false
+    BRANDS.each do |item|
+      if item == brand
+        already_exists = true
+      end
+    end
+    if already_exists == false
+      BRANDS << brand
+    end
   end
 
   def cobble
